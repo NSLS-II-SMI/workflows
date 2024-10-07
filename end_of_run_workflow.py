54e585c20bbefcb2e223347f856107cd6fd0fb0c
Juan Marulanda <jmaruland@bnl.gov>
2024-10-07 13:31:03 -0400

    whitespace

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,10 +1,12 @@
 from prefect import task, flow, get_run_logger
 from data_validation import data_validation
 
+
 @task
 def log_completion():
     logger = get_run_logger()
     logger.info("Complete")
+
 
 @flow
 def end_of_run_workflow(stop_doc):
