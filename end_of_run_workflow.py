82f1c82ec7d30b59573ea151f48e09ee9ee86153
Dan Allan <dallan@bnl.gov>
2025-02-21 10:37:51 -0500

    Execute end-of-run tasks concurrently.

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -13,6 +13,10 @@ def log_completion():
 @flow(task_runner=ConcurrentTaskRunner())
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
-    data_validation(uid)
-    linker(uid)
+    # Launch validation and linker concurrently.
+    validation_task = data_validation.submit(uid)
+    linker_tasks linker.submit(uid)
+    # Wait for completion.
+    validation_task.result()
+    linker_task.result()
     log_completion()
