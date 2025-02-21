fd5f9e5ab14093e5075305e94e6c4c7919f0bcd9
Dan Allan <dallan@bnl.gov>
2025-02-21 17:03:44 -0500

    Fix concurrency by using tasks not flows.

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -1,7 +1,7 @@
 from prefect import task, flow, get_run_logger
 from prefect.task_runners import ConcurrentTaskRunner
-from data_validation import data_validation
-from linker import linker
+from data_validation import read_all_streams
+from linker import get_symlink_pairs
 
 
 @task
@@ -12,13 +12,18 @@ def log_completion():
 
 @flow(task_runner=ConcurrentTaskRunner())
 def end_of_run_workflow(stop_doc):
+    logger = get_run_logger()
     uid = stop_doc["run_start"]
 
     # Launch validation and linker concurrently.
-    validation_task = data_validation.submit(uid)
-    linker_task = linker.submit(uid)
+    det_map = {"900KW": "WAXS", "1M": "SAXS"}
+    linker_task = get_symlink_pairs.submit(uid, det_map=det_map)
+    logger.info("Launched linker task")
+    validation_task = read_all_streams.submit(uid, beamline_acronym="smi")
+    logger.info("Launched validation task")
 
     # Wait for completion.
+    logger.info("Waiting for tasks to complete")
     validation_task.result()
     linker_task.result()
 
