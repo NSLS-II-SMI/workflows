2145b164c27a54bc2598034f6b014513b611ac7f
Eugene <ymatviych@bnl.gov>
2025-02-21 13:21:59 -0500

    Fix typo

--- end_of_run_workflow.py
+++ end_of_run_workflow.py
@@ -13,10 +13,13 @@ def log_completion():
 @flow(task_runner=ConcurrentTaskRunner())
 def end_of_run_workflow(stop_doc):
     uid = stop_doc["run_start"]
+
     # Launch validation and linker concurrently.
     validation_task = data_validation.submit(uid)
-    linker_tasks linker.submit(uid)
+    linker_task = linker.submit(uid)
+
     # Wait for completion.
     validation_task.result()
     linker_task.result()
+
     log_completion()
