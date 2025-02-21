62656d256da3ffb2abc2ade9659c576a57894299
EliotGann <egann@bnl.gov>
2025-02-21 16:00:01 -0500

    adding optional sample name from primary stream

--- linker.py
+++ linker.py
@@ -81,6 +81,7 @@ def get_symlink_pairs(ref, *, det_map, root_map=None):
 
     links = []
     target_template: str
+    sample_name: str
     output_path: str
     resource_info = {}
     datum_info = {}
@@ -92,8 +93,8 @@ def get_symlink_pairs(ref, *, det_map, root_map=None):
     for name, doc in hrf.documents():
         if name == "start":
             start_uid = doc["uid"]
-
-            target_template = (f"{{det_name}}/{doc['username']}_{doc['sample_name']}_"
+            sample_name = doc['sample_name']
+            target_template = (f"{{det_name}}/{doc['username']}_{{sample}}_"
                                f"id{doc['scan_id']}_{{N:06d}}_{{det_type}}.tif")
 
             target_path = Path(
@@ -136,6 +137,10 @@ def get_symlink_pairs(ref, *, det_map, root_map=None):
             if name == "event":
                 doc = event_model.pack_event_page(doc)
 
+            if "target_file_name" in target_keys:
+                sample = doc['data']['target_file_name']
+            else:
+                sample = sample_name
             for key in target_keys:
 
                 det, _, _ = key.partition("_")
@@ -166,6 +171,7 @@ def get_symlink_pairs(ref, *, det_map, root_map=None):
                             det_name=det_name,
                             N=point_number * fpp + fr,
                             det_type=det_type,
+                            sample=sample,
                         )
                         links.append(
                             (start_uid, source_path, dest_path, analysis_path)
